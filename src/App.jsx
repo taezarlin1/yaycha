8a9125cdcdf3714be796570d57ef640e0e2ef5a2
Alik <taezarlin1@gmail.com>
2025-02-28 11:35:19 +0630

    Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,55 @@
-function Item({ content, name }){
-  return (
-    <li style={{padding: 10, borderBottom: "1px solid #ddd"}}>
-      {content} - <b>{name}</b>
-    </li>
-  )
-}
-
-function List({ children }){
-  return <ul
-    style={{
-      listStyle: "none",
-      padding: 0,
-      margin: 0,
-      border: "1px solid #ddd",
-      borderRadius: 8,
-      overflow: "hidden"
-    }}
-  >
-    {children}
-  </ul>
-}
+import { useState } from "react"
+import Item from "./Item"
+import List from "./List"
+import Form from "./Form"
 
 export default function App(){
+  const [data, setData] = useState([
+    {id: 1, content: "Hello World!", name: "Alice"},
+    {id: 2, content: "React is fun", name: "Bob"},
+    {id: 3, content: "Interesting", name: "Chris"},
+
+  ])
+
+  const [showForm, setShowForm] = useState(false);
+
+  const remove = (id)=>{
+    setData(data.filter(item=>item.id!==id))
+  }
+  const add = (content, name)=>{
+    const id = data[data.length - 1].id + 1;
+    setData([...data, {id, content, name}])
+  }
+
   return (
     <div style={{ maxWidth: 600, margin: "20px auto"}}>
-      <h1>Yaycha</h1>
+      <h1
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center"
+        }}
+      >Yaycha
+      <button 
+        onClick={()=>setShowForm(!showForm)}
+        style={{
+          width: 32,
+          height: 32,
+          borderRadius: 50,
+          border: "0 none",
+          background: showForm ? "#dc3545" : "#0d6efd",
+          color: "white"
+        }}
+      >
+        {showForm ? "x" : "+"}
+      </button>
+      </h1>
+      
+      {showForm && <Form add={add}/>}
       <List>
-        <Item content="Hello, World!" name="Alice"/>
-        <Item content="React is fun!" name="Bob"/>
-
+        {data.map(item=>{
+          return <Item key={item.id} item={item} remove={remove}/>
+        })}
       </List>
     </div>
   )
